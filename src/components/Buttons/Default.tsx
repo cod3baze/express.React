1d6ab22265b8ca70c52b72fc74538f10fe159b6d
elias alexandre <elliasallex@gmail.com>
2022-06-27 15:12:37 -0300

    default button ui

--- src/components/Buttons/Default.tsx
+++ src/components/Buttons/Default.tsx
@@ -1,20 +1,30 @@
 import { ButtonHTMLAttributes } from "react";
 
-import "./styles.module.scss";
+import styles from "./styles.module.scss";
 
 type IButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  text: string;
   isOutlined?: boolean;
+  isLoading?: boolean;
 };
 
-const Button = ({ isOutlined = false, ...rest }: IButtonProps) => {
+const Button = ({
+  isOutlined = false,
+  isLoading = false,
+  text,
+  ...rest
+}: IButtonProps) => {
   const { type } = rest;
 
   return (
     <button
-      className={`button ${isOutlined ? "outlined" : ""}`}
+      className={`${styles.button} ${isOutlined ? styles.outlined : ""}`}
+      disabled={isLoading}
       {...rest}
       type={type ? "submit" : "button"}
-    />
+    >
+      {isLoading ? "Loading..." : text}
+    </button>
   );
 };
 
